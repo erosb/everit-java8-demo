e22d0ab49eed0f1b6738c27385c7a166048ec64f
bence.eros <bence.eros@everit.biz>
2014-05-30 16:01:05 +0200

    adding UserDAO with supplier example

---
+++ src/main/java/org/everit/demo/java8/UserDAO.java
@@ -0,0 +1,42 @@
+package org.everit.demo.java8;
+
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Objects;
+import java.util.SortedMap;
+import java.util.TreeMap;
+import java.util.function.Supplier;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+
+public class UserDAO {
+
+    private final Collection<User> users;
+
+    private final Supplier<Collection<User>> collSupplier;
+
+    public UserDAO(final Collection<User> users, final Supplier<Collection<User>> collSupplier) {
+        this.users = Objects.requireNonNull(users, "users cannot be null");
+        this.collSupplier = Objects.requireNonNull(collSupplier, "collSupplier cannot be null");
+    }
+    
+    public SortedMap<Integer, Collection<User>> groupUsersByAge() {
+        SortedMap<Integer, Collection<User>> rval = new TreeMap<>();
+        for (User users : users) {
+            Collection<User> peopleByAge = rval.get(users.getAge());
+            if (peopleByAge == null) {
+                peopleByAge = collSupplier.get();
+                rval.put(users.getAge(), peopleByAge);
+            }
+            peopleByAge.add(users);
+        }
+        return rval;
+    }
+
+
+}
